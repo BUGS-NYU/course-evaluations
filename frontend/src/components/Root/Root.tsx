854721ff97f1e55a38994d331696ee299a2af2dc
nedjulius <julius.nedzinskas@gmail.com>
2023-04-27 18:44:32 -0400

    Add Course Entry

--- frontend/src/components/Root/Root.tsx
+++ frontend/src/components/Root/Root.tsx
@@ -1,13 +1,26 @@
+import { FunctionalComponent } from 'react';
 import { usePhraseQueryState } from 'lib/hooks.ts';
+import { BaseCourse } from 'lib/types.ts';
 import { useSearch } from './use-search.ts';
 import { PageCounter } from './PageCounter';
 import styles from './styles.module.css';
+
+const CourseEntry: FunctionalComponent<BaseCourse> = ({ _id, term, description, instructors }) => {
+  return (
+    <div className={styles.courseEntry}>
+      <h1>{description}</h1>
+    </div>
+  );
+};
 
 const Root = () => {
   const [phrase, setPhrase] = usePhraseQueryState();
   const { data, isLoading } = useSearch();
 
-  const renderData = data.length > 0 ? 'data' : 'no data';
+  const renderData =
+    data.data.length > 0
+      ? data.data.map((course) => <CourseEntry key={course._id} {...course} />)
+      : `No results to display`;
 
   const setSearchValue = (e) => setPhrase(e.target.value);
 
@@ -16,12 +29,13 @@ const Root = () => {
       <div className={styles.searchInputBg}>
         <input
           type="text"
+          placeholder="Type your search phrase..."
           value={phrase}
           onChange={setSearchValue}
           className={styles.searchInput}
         />
       </div>
-      {isLoading ? 'loading...' : renderData}
+      {isLoading ? 'Loading...' : renderData}
       <PageCounter totalPages={data.pagination.totalPages} />
     </div>
   );
