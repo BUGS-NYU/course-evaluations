2441f3929430c8533553e05ebe9731c9e4c12d56
David Mo <44306450+unknown@users.noreply.github.com>
2023-04-28 12:15:20 -0400

    Migrate to ts-node

--- scraper/src/config.ts
+++ scraper/src/config.ts
@@ -6,7 +6,7 @@ interface Config {
 
 const config: Config = {
   strictMode: false, // if true, the scraper will stop if it encounters courses that don't have any data
-  devMode: false, // if true, the scraper will launch with a headed browser and log additional data
+  devMode: true, // if true, the scraper will launch with a headed browser and log additional data
   terms: ["1228"], // if not undefined, the scraper will scrape only these terms. otherwise, it will scrape all terms
 };
 
